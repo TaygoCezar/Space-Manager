fee97448832ec90439c92f48096d35cf56ba7e32
taygo <taygocezar@gmail.com>
2025-03-04 18:26:35 -0300

    início da interface

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ from flet import Page
 import router
 
 def main(page: Page) -> None:
+    page.fonts = {
+        "InstrumentsSans-Bold": "/fonts/InstrumentsSans-Bold.ttf",
+    }
+    page.theme_mode = "light"
+
     router.init(page)
     page.on_route_change = router.navigate
     page.go("home")
--- routes/home.py
+++ routes/home.py
@@ -1,10 +1,34 @@
 from flet import Page
-from flet import View, Text
+from flet import View, Container, Row, Text
+from flet import padding, FontWeight
 
 def home(page: Page) -> View:
     return View(
         "home",
         [
-            Text("olá mundo")
-        ]
+            Container(
+                Row([
+                    Text("Space Manager", **styles["navigation-title"])
+                ]),
+                **styles["navigation-container"]
+            )   
+        ],
+        **styles["view"]
     )
+
+styles = {
+    "view": {
+        "padding": padding.all(0)
+    },
+
+    "navigation-container": {
+        "padding": padding.symmetric(20, 40),
+        "bgcolor": "#98D4C6",
+    },
+    "navigation-title": {
+        "font_family": "InstrumentSans-Bold",
+        "size": 35,
+        "weight": FontWeight.BOLD,
+        "color": "#ffffff"
+    }
+}
